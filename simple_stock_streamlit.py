da0599f3c9470b4a6a5162b74a70748f502ef29a
Ram Dershan <ram.dershan@rocketmail.com>
2023-02-20 18:39:47 -0800

    update to readme

--- simple_stock_streamlit.py
+++ simple_stock_streamlit.py
@@ -5,11 +5,14 @@ import pandas as pd
 st.header('Simple Stock Price App')
 st.subheader('Shown are the stock closing price and volume of TJX Companies Inc.')
 
+#define ticker symbol
 tickerSymbol =  'TJX'
 
+#get data
 tickerData = yf.Ticker(tickerSymbol)
 
-tickerDf = tickerData.history(period='1d', start = '2022-1-1', end='2023-1-1')
+#get historical prices 
+tickerDf = tickerData.history(period='1d', start = '2022-1-1', end = '2023-1-1')
 
 st.line_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
